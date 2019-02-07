e232cb4e3c5801507a33881d82ced31bdfe9f883
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-02-07 14:27:26 +0000

    Don't spawn extra process on every admin-pipe.sh

--- rootfs/web-admin/admin-pipe.sh
+++ rootfs/web-admin/admin-pipe.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 HERE=$(dirname $0)
-# If admin-server isn't started, then start it now and retry
-nc 127.0.0.1 8080 2> /dev/null || (
+if ! nc 127.0.0.1 8080 2> /dev/null; then
+    # If admin-server isn't started, then start it now and retry
     $HERE/admin-server.py
     nc 127.0.0.1 8080
-)
+fi
