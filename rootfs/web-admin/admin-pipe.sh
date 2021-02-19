4dda624fcfe7a1a3e1bd675135e7c05bd6fe29e2
Tom Cobb <tom.cobb@diamond.ac.uk>
2021-02-19 11:46:03 +0000

    Fixed web-admin startup race

--- rootfs/web-admin/admin-pipe.sh
+++ rootfs/web-admin/admin-pipe.sh
@@ -2,6 +2,8 @@
 HERE=$(dirname $0)
 if ! nc 127.0.0.1 8080 2> /dev/null; then
     # If admin-server isn't started, then start it now and retry
-    $HERE/admin-server.py
+    # This might fail if another one is already starting up, so
+    # log any errors and still continue with the netcat
+    $HERE/admin-server.py 2>> /var/log/web-admin.log
     nc 127.0.0.1 8080
 fi
