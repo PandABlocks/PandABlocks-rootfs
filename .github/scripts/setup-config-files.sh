a766bfef7e08e07570a20ed345dfbaf948525a13
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-07-07 13:31:31 +0100

    Update shell script with new paths and file creation methods

--- .github/scripts/setup-config-files.sh
+++ .github/scripts/setup-config-files.sh
@@ -1,39 +1,42 @@
-# !/bin/bash
+#!/bin/bash
 # Generates config files in PandABlocks-rootfs and rootfs repositories and populates them with information.
 
+GITHUB_WORKSPACE='/home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs'
+
 # PandABlocks-rootfs:
-CONFIG_PANDABLOCKS-ROOTFS = "
+# Create the CONFIG file
+cd $GITHUB_WORKSPACE/pandABlocks-rootfs
+touch CONFIG
+# Populate the CONFIG file
+cat >> CONFIG <<EOL
 # Location of rootfs builder
-ROOTFS_TOP = ${{ github.workspace }}/rootfs
+ROOTFS_TOP = /home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs/rootfs
 
 # Toolchain used to build the target
-TOOLCHAIN_ROOT = ${{ github.workspace }}/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+TOOLCHAIN_ROOT = /home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
 
 # Where to find source files
-TAR_FILES = ${{ github.workspace }}/tar-files
+TAR_FILES = /home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs/tar-files
 
 # Target location for build
-PANDA_ROOT = ${{ github.workspace }}/build
+PANDA_ROOT = /home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs/build
 
 # Whether the platform is zynq or zyqnmp
-PLATFORM = zynq"
+PLATFORM = zynq
+EOL
 
-# Create and populate the CONFIG file
-cd ${{ github.workspace }}/pandABlocks-rootfs
-touch CONFIG
-echo CONFIG_PANDABLOCKS-ROOTFS > CONFIG
 
 # rootfs:
-CONFIG_ROOTFS = "
+# Create the CONFIG file
+cd $GITHUB_WORKSPACE/rootfs
+touch CONFIG.local
+# Populate the CONFIG file
+cat >> CONFIG.local <<EOL
 TARGET = minimal
 
 # This is the location where source and build files will be placed.
-ROOTFS_ROOT = ${{ github.workspace }}/build
+ROOTFS_ROOT = /home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs/build
 
 # This is where all of the source tar files will be found.
-TAR_DIRS = ${{ github.workspace }}/tar-files"
-
-# Create and populate the CONFIG file
-cd ${{ github.workspace }}/rootfs
-touch CONFIG.local
-echo CONFIG_ROOTFS > CONFIG.local
+TAR_DIRS = /home/runner/work/PandABlocks-rootfs/PandABlocks-rootfs/tar-files
+EOL
