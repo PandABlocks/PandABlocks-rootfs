4ed3a3491d33fab18720aa20be4b1cd211d745b7
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-07-12 13:18:57 +0100

    remove \ from tar-file list

--- .github/scripts/tar-files.sh
+++ .github/scripts/tar-files.sh
@@ -34,5 +34,4 @@ curl -O -L https://github.com/libffi/libffi/releases/tag/v3.3-rc2 \
      -O -L https://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz \
      -O -L https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz \
      -o ncurses-6.1.tar.gz -L https://github.com/mirror/ncurses/archive/refs/tags/v6.1.tar.gz \
-     #-O -L https://gitlab.freedesktop.org/pkg-config/pkg-config/-/archive/pkg-config-0.28/pkg-config-pkg-config-0.28.tar.gz \
-     /
+     -O -L https://gitlab.freedesktop.org/pkg-config/pkg-config/-/archive/pkg-config-0.28/pkg-config-pkg-config-0.28.tar.gz
