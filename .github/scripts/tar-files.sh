edc4fa60a87192a0178721ca31e9c115db0f8a16
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-07-23 09:49:31 +0100

    Revert m4 to 1.4.18

--- .github/scripts/tar-files.sh
+++ .github/scripts/tar-files.sh
@@ -28,7 +28,7 @@ curl -OL https://github.com/libffi/libffi/releases/download/v3.3-rc2/libffi-3.3-
      -OL https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz \
      -OL https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz \
      -OL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.0.2.tar.gz \
-     -OL https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz \
+     -OL https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz \
      -OL https://files.pythonhosted.org/packages/11/0a/7f13ef5cd932a107cd4c0f3ebc9d831d9b78e1a0e8c98a098ca17b1d7d97/setuptools-41.6.0.zip \
      -OL http://jdelvare.nerim.net/mirror/i2c-tools/i2c-tools-3.1.1.tar.bz2 \
      -OL https://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz \
@@ -41,5 +41,6 @@ curl -OL https://github.com/libffi/libffi/releases/download/v3.3-rc2/libffi-3.3-
 # Adjust checksums to the new versioned packages (temporary)
 cd $GITHUB_WORKSPACE/rootfs/scripts/makefiles
 # Addressed bug encountered with v1.4.18 (Bug: "Please port gnulib freadahead.c to your platform!")
-sed -i 's/3b53feb7063fea08ed47e874ac5ce802/f4a2b0284d80353b995f8ef2385ed73c/g' TOOLKIT_DEFS
-sed -i 's/1.4.18/1.4.19/g' TOOLKIT_DEFS
+# sed -i 's/3b53feb7063fea08ed47e874ac5ce802/f4a2b0284d80353b995f8ef2385ed73c/g' TOOLKIT_DEFS #v1.4.19
+sed -i 's/3b53feb7063fea08ed47e874ac5ce802/a077779db287adf4e12a035029002d28/g' TOOLKIT_DEFS # v1.4.18
+# sed -i 's/1.4.18/1.4.19/g' TOOLKIT_DEFS
