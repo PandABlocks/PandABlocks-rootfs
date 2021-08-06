90e999b4b17c63f9b23544d204226c0b0bfb0bcf
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-06 09:39:52 +0100

    Remove manual update of m4 (1.4.19) due to rootfs pull-request merge

--- .github/scripts/tar-files.sh
+++ .github/scripts/tar-files.sh
@@ -29,7 +29,7 @@ if [[ -z $1 ]]; # If no argument is given
           -OL https://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz \
           -OL https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz \
           -OL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.0.2.tar.gz \
-          -OL https://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz \
+          -OL https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz \
           -OL https://files.pythonhosted.org/packages/11/0a/7f13ef5cd932a107cd4c0f3ebc9d831d9b78e1a0e8c98a098ca17b1d7d97/setuptools-41.6.0.zip \
           -OL http://jdelvare.nerim.net/mirror/i2c-tools/i2c-tools-3.1.1.tar.bz2 \
           -OL https://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz \
@@ -42,10 +42,3 @@ fi
 # Install linux tar files
 curl -o u-boot-xlnx-xilinx-v2020.2.2-k26.tar.gz -L https://github.com/Xilinx/u-boot-xlnx/archive/refs/tags/xilinx-v2020.2.2-k26.tar.gz \
      -o linux-xlnx-xilinx-v2020.2.2-k26.tar.gz -L https://github.com/Xilinx/linux-xlnx/archive/refs/tags/xilinx-v2020.2.2-k26.tar.gz
-
-# Adjust checksums to the new versioned packages (temporary)
-cd ../rootfs/scripts/makefiles
-# Addressed bug encountered with v1.4.18 (Bug: "Please port gnulib freadahead.c to your platform!")
-# sed -i 's/3b53feb7063fea08ed47e874ac5ce802/f4a2b0284d80353b995f8ef2385ed73c/g' TOOLKIT_DEFS #v1.4.19
-sed -i 's/3b53feb7063fea08ed47e874ac5ce802/a077779db287adf4e12a035029002d28/g' TOOLKIT_DEFS # v1.4.18
-# sed -i 's/1.4.18/1.4.19/g' TOOLKIT_DEFS
