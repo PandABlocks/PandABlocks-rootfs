171737a1dd277a517e0e8cdcb15ebd5a423cf051
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-19 10:43:00 +0100

    Remove literal from CONFIG file

--- .github/scripts/config-file-rootfs.sh
+++ .github/scripts/config-file-rootfs.sh
@@ -11,21 +11,21 @@ elif [ "$PLATFORM" == "zynqmp" ]; then
 fi
 
 # PandABlocks-rootfs:
-cat >> PandABlocks-rootfs/CONFIG << 'EOL'
+cat >> PandABlocks-rootfs/CONFIG << EOL
 # Location of rootfs builder
-ROOTFS_TOP = $(GITHUB_WORKSPACE)/rootfs
+ROOTFS_TOP = \$GITHUB_WORKSPACE/rootfs
 
 # Toolchain used to build the target
-TOOLCHAIN_ROOT = $(GITHUB_WORKSPACE)/$TOOLCHAIN
+TOOLCHAIN_ROOT = \$GITHUB_WORKSPACE/$TOOLCHAIN
 
 # Where to find source files
-TAR_FILES = $(GITHUB_WORKSPACE)/tar-files
+TAR_FILES = \$GITHUB_WORKSPACE/tar-files
 
 # Target location for build
-PANDA_ROOT = $(GITHUB_WORKSPACE)/build
+PANDA_ROOT = \$GITHUB_WORKSPACE/build
 
 # Whether the platform is zynq or zyqnmp
-PLATFORM = $(PLATFORM)
+PLATFORM = $PLATFORM
 EOL
 
 # rootfs:
@@ -33,8 +33,8 @@ cat >> rootfs/CONFIG.local << 'EOL'
 TARGET = minimal
 
 # This is the location where source and build files will be placed.
-ROOTFS_ROOT = $(GITHUB_WORKSPACE)/build
+ROOTFS_ROOT = $GITHUB_WORKSPACE/build
 
 # This is where all of the source tar files will be found.
-TAR_DIRS = $(GITHUB_WORKSPACE)/tar-files
+TAR_DIRS = $GITHUB_WORKSPACE/tar-files
 EOL
