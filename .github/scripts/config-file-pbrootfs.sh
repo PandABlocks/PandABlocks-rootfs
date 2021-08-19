ab39212735f0e49638addfa9a730650b48f263ab
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-19 10:54:21 +0100

    Split pbrootfs and rootfs config generation scripts

---
+++ .github/scripts/config-file-pbrootfs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Generates and populates CONFIG file for PandABlocks-rootfs repo.
+
+PLATFORM=$1
+
+# Determine the toolchain to use
+if [ "$PLATFORM" == "zynq" ]; then
+    TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+elif [ "$PLATFORM" == "zynqmp" ]; then
+    TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+fi
+
+cat >> PandABlocks-rootfs/CONFIG << EOL
+# Location of rootfs builder
+ROOTFS_TOP = \$(GITHUB_WORKSPACE)/rootfs
+
+# Toolchain used to build the target
+TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/$TOOLCHAIN
+
+# Where to find source files
+TAR_FILES = \$(GITHUB_WORKSPACE)/tar-files
+
+# Target location for build
+PANDA_ROOT = \$(GITHUB_WORKSPACE)/build
+
+# Whether the platform is zynq or zyqnmp
+PLATFORM = $PLATFORM
+EOL
--- .github/scripts/config-file-rootfs.sh
+++ .github/scripts/config-file-rootfs.sh
@@ -1,40 +1,12 @@
 #!/bin/bash
-# Generates config files in PandABlocks-rootfs and rootfs repositories and populates them with information.
+# Generates and populates CONFIG file for rootfs repo.
 
-PLATFORM=$1
-
-# Determine the toolchain to use
-if [ "$PLATFORM" == "zynq" ]; then
-    TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
-elif [ "$PLATFORM" == "zynqmp" ]; then
-    TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
-fi
-
-# PandABlocks-rootfs:
-cat >> PandABlocks-rootfs/CONFIG << EOL
-# Location of rootfs builder
-ROOTFS_TOP = \$GITHUB_WORKSPACE/rootfs
-
-# Toolchain used to build the target
-TOOLCHAIN_ROOT = \$GITHUB_WORKSPACE/$TOOLCHAIN
-
-# Where to find source files
-TAR_FILES = \$GITHUB_WORKSPACE/tar-files
-
-# Target location for build
-PANDA_ROOT = \$GITHUB_WORKSPACE/build
-
-# Whether the platform is zynq or zyqnmp
-PLATFORM = $PLATFORM
-EOL
-
-# rootfs:
 cat >> rootfs/CONFIG.local << 'EOL'
 TARGET = minimal
 
 # This is the location where source and build files will be placed.
-ROOTFS_ROOT = $GITHUB_WORKSPACE/build
+ROOTFS_ROOT = $(GITHUB_WORKSPACE)/build
 
 # This is where all of the source tar files will be found.
-TAR_DIRS = $GITHUB_WORKSPACE/tar-files
+TAR_DIRS = $(GITHUB_WORKSPACE)/tar-files
 EOL
