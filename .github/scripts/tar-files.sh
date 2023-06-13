5f60aeca9fa292470cf847e0de6ba55a236caa77
Emilio Perez <emiliopeju@gmail.com>
2023-06-13 17:40:00 +0100

    Update non-python related packages to latest versions

--- .github/scripts/tar-files.sh
+++ .github/scripts/tar-files.sh
@@ -9,30 +9,28 @@ cd /tar-files
 if [[ -z $1 ]]; then
     # Install tar file rootfs dependencies
     curl -OL https://github.com/libffi/libffi/releases/download/v3.3-rc2/libffi-3.3-rc2.tar.gz \
-        -OL https://ftp.gnu.org/gnu/nano/nano-2.4.1.tar.gz \
         -o cothread-2-18.tar.gz -L https://github.com/dls-controls/cothread/archive/refs/tags/2-18.tar.gz \
         -OL https://zlib.net/fossils/zlib-1.2.8.tar.gz \
-        -OL https://src.fedoraproject.org/repo/pkgs/ethtool/ethtool-2.6.36.tar.gz/3b2322695e9ee7bf447ebcdb85f93e83/ethtool-2.6.36.tar.gz \
+        -OL https://www.netfilter.org/projects/libmnl/files/libmnl-1.0.5.tar.bz2 \
+        -OL https://mirrors.edge.kernel.org/pub/software/network/ethtool/ethtool-6.3.tar.gz \
         -OL https://infraroot.at/pub/mtd/mtd-utils-2.1.2.tar.bz2 \
         -OL https://busybox.net/downloads/busybox-1.23.2.tar.bz2 \
-        -OL http://kmq.jp.distfiles.macports.org/procServ/procServ-2.6.0.tar.gz \
+        -OL https://github.com/ralphlange/procServ/releases/download/v2.8.0/procServ-2.8.0.tar.gz \
         -OL https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz \
         -OL https://qa.debian.org/watch/sf.php/e2fsprogs/e2fsprogs-1.46.2.tar.gz \
         -OL https://github.com/numpy/numpy/releases/download/v1.17.5/numpy-1.17.5.tar.gz \
         -OL http://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/ntp-4.2.8p15.tar.gz \
-        -OL https://mirror.dropbear.nl/mirror/releases/dropbear-2015.67.tar.bz2 \
-        -OL https://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz \
-        -OL https://busybox.net/downloads/busybox-1.33.1.tar.bz2 \
+        -OL https://mirror.dropbear.nl/mirror/releases/dropbear-2022.83.tar.bz2 \
+        -OL https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz \
+        -OL https://busybox.net/downloads/busybox-1.36.1.tar.bz2 \
         -o tornado-6.0.3.tar.gz -L https://github.com/tornadoweb/tornado/archive/refs/tags/v6.0.3.tar.gz \
         -OL https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.gz \
         -OL https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz \
         -OL https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.0.2.tar.gz \
         -OL https://ftp.gnu.org/gnu/m4/m4-1.4.19.tar.gz \
         -OL https://files.pythonhosted.org/packages/11/0a/7f13ef5cd932a107cd4c0f3ebc9d831d9b78e1a0e8c98a098ca17b1d7d97/setuptools-41.6.0.zip \
-        -OL http://oe-lite.org/mirror/i2c-tools/i2c-tools-3.1.1.tar.bz2 \
-        -OL https://ftp.gnu.org/gnu/screen/screen-4.2.1.tar.gz \
         -OL https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz \
-        -OL https://ftp.gnu.org/gnu/ncurses/ncurses-6.1.tar.gz \
+        -OL https://ftp.gnu.org/gnu/ncurses/ncurses-6.4.tar.gz \
         -OL https://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz
 
 fi
