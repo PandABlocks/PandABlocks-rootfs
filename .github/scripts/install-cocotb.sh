86d1ac9a24d0e33d79bbff5f51afea2c35655a0a
Jacob Williamson <jacob.williamson@diamond.ac.uk>
2024-11-19 16:11:20 +0000

    Making container use python 3.12

--- .github/scripts/install-cocotb.sh
+++ .github/scripts/install-cocotb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export PATH=$PATH:/usr/bin:/usr/sbin
 cd /tmp && \
-git clone -b "elab-args" https://github.com/jacob720/cocotb && \
-cd cocotb
-python3.12 -m pip install .
+git clone https://github.com/cocotb/cocotb && \
+cd cocotb && \
+git reset --hard 6649d76
+python3 -m pip install .
